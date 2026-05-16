cdc945ef2b74b7d5525aceda32781d4e12f689ad
Scorbutics <brice.bulgarelli@gmail.com>
2026-05-16 18:22:24 +1000

    fix: compilation error on android

--- src/SFML/Window/Android/HostedAndroidWindow.cpp
+++ src/SFML/Window/Android/HostedAndroidWindow.cpp
@@ -33,7 +33,14 @@
 #include <SFML/System/Err.hpp>
 
 #include <android/native_window.h>
+
+// We check for this definition in order to avoid multiple definitions of GLAD
+// entities during unity builds of SFML.
+#ifndef SF_GLAD_EGL_IMPLEMENTATION_INCLUDED
+#define SF_GLAD_EGL_IMPLEMENTATION_INCLUDED
+#define SF_GLAD_EGL_IMPLEMENTATION
 #include <glad/egl.h>
+#endif
 
 #include <cstddef>
 
